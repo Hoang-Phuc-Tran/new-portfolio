18b505f67bf9020550a99df64eea3591f3634315
ptran9102 <hoangphuctran9102@gmail.com>
2023-12-09 22:14:56 -0500

    Deploy 09-12

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -17,7 +17,7 @@ const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My recent Work</h5>
-      <h2>Portfolio</h2>
+      <h2>My Experience</h2>
 
       <div className="container portfolio__container">
         <article className="portfolio__item">
